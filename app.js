8dcf53f7de294f10ad9c543e3598b34caf5a0ff4
bowo-prasetyo <bpraz@yahoo.com>
2026-05-16 09:24:35 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,147 @@
 const Home = {
+  template: `
+  <div class="container">
+
+    <h1>Browser Genetic Programming</h1>
+
+    <button @click="start">Start Evolution</button>
+    <button @click="stop">Stop</button>
+
+    <div class="stats">
+      <div><b>Best Fitness:</b> {{ bestFitness }}</div>
+      <div><b>Best Program:</b></div>
+
+      <div class="code">
+        {{ expression }}
+      </div>
+    </div>
+
+    <canvas ref="canvas" width="600" height="300"></canvas>
+
+    <svg width="400" height="200">
+
+      <line x1="200" y1="50" x2="120" y2="130" stroke="white"/>
+      <line x1="200" y1="50" x2="280" y2="130" stroke="white"/>
+
+      <circle cx="200" cy="50" r="20" fill="orange"/>
+      <text x="195" y="55" fill="black">{{ best.op }}</text>
+
+      <circle cx="120" cy="130" r="20" fill="skyblue"/>
+      <text x="115" y="135" fill="black">{{ best.left }}</text>
+
+      <circle cx="280" cy="130" r="20" fill="lightgreen"/>
+      <text x="275" y="135" fill="black">{{ best.right }}</text>
+
+    </svg>
+
+  </div>
+  `,
+
+  data() {
+    return {
+      worker: null,
+      best: {
+        left: 'x',
+        op: '+',
+        right: '1'
+      },
+      bestFitness: 0,
+      db: null
+    };
+  },
+
+  computed: {
+    expression() {
+      return `${this.best.left} ${this.best.op} ${this.best.right}`;
+    }
+  },
+
+  mounted() {
+    this.initDB();
+    this.drawCanvas();
+  },
+
+  methods: {
+
+    initDB() {
+      const request = indexedDB.open('gp-db', 1);
+
+      request.onupgradeneeded = (e) => {
+        const db = e.target.result;
+
+        if (!db.objectStoreNames.contains('bestPrograms')) {
+          db.createObjectStore('bestPrograms', {
+            keyPath: 'id',
+            autoIncrement: true
+          });
+        }
+      };
+
+      request.onsuccess = (e) => {
+        this.db = e.target.result;
+      };
+    },
+
+    saveBest() {
+      if (!this.db) return;
+
+      const tx = this.db.transaction(['bestPrograms'], 'readwrite');
+      const store = tx.objectStore('bestPrograms');
+
+      store.add({
+        timestamp: Date.now(),
+        program: this.best,
+        fitness: this.bestFitness
+      });
+    },
+
+    start() {
+      if (this.worker) {
+        this.worker.terminate();
+      }
+
+      this.worker = new Worker('worker.js');
+
+      this.worker.onmessage = (e) => {
+        const msg = e.data;
+
+        if (msg.type === 'update') {
+          this.best = msg.best;
+          this.bestFitness = msg.fitness;
+
+          this.drawCanvas();
+          this.saveBest();
+        }
+      };
+
+      this.worker.postMessage({ type: 'start' });
+    },
+
+    stop() {
+      if (!this.worker) return;
+
+      this.worker.postMessage({ type: 'stop' });
+    },
+
+    evaluate(x) {
+      const left = this.best.left === 'x'
+        ? x
+        : Number(this.best.left);
+
+      const right = this.best.right === 'x'
+        ? x
+        : Number(this.best.right);
+
+      switch (this.best.op) {
+        case '+': return left + right;
+        case '-': return left - right;
+        case '*': return left * right;
+      }
+
+      return 0;
+    },
+
+    drawCanvas() {
       const canvas = this.$refs.canvas;
 
       if (!canvas) return;
